39f0787d6069d55eea559f5d2227b6787372779e
wongyongren <wongyongren@hotmail.com>
2021-11-19 18:56:22 +0800

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,4 +1,9 @@
 <template>
+<div style="text-align: left"> 
+  <a href="/">Main Page </a>
+  <a href="/login">Login Page </a>
+  <a href="/report">Report Page </a>
+</div>
   <div style="text-align: right">
     <span class="mx-2">{{ email }} </span>
     <button @click="logOut" color="cust-secondary">sign out</button>
