62b3ab812c7f9d692a4c033d5f926eb49b8217d1
wongyongren <wongyongren@hotmail.com>
2021-11-19 17:14:51 +0800

    update

--- src/components/ReportList.vue
+++ src/components/ReportList.vue
@@ -1,12 +1,19 @@
 <template >
-  <div v-for="site in sites" :key="site">
-    <br />
+  <div v-for="site in sites" :key="site" class="row box">
     <h3>{{ site.location }}</h3>
-    <div v-for="name in sites.worker" :key="name">
-      {{name}}
-      <li>{{ site.worker[0].name }}</li>
+
+    <div v-for="worker in site.worker" :key="worker" class="">
+      <!--time in -->
+      <div v-if="worker.timeIn != null && worker.timeOut == null">
+        <li>{{ worker.name }} - {{ worker.timeIn }}</li>
+      </div>
+      <!--time in & out -->
+      <div v-if="worker.timeOut != null">
+        <li>{{ worker.name }} - {{ worker.timeIn }} - {{ worker.timeOut }}</li>
+      </div>
     </div>
   </div>
+  <button v-print>Print the entire page</button>
 </template>
 
 <script>
@@ -20,10 +27,26 @@ export default {
             {
               id: 1,
               name: "Wong",
+              timeIn: "10:05:15 AM",
+              timeOut: "12:05:15 PM",
             },
             {
               id: 2,
               name: "ABC",
+              timeIn: "10:05:15 AM",
+              timeOut: null,
+            },
+            {
+              id: 3,
+              name: "Wong",
+              timeIn: "10:05:15 AM",
+              timeOut: null,
+            },
+            {
+              id: 4,
+              name: "ABC",
+              timeIn: "10:05:15 AM",
+              timeOut: "11:05:15 AM",
             },
           ],
         },
@@ -33,10 +56,14 @@ export default {
             {
               id: 1,
               name: "Wong",
+              timeIn: "123",
+              timeOut: "123",
             },
             {
               id: 2,
               name: "ABC",
+              timeIn: "123",
+              timeOut: "123",
             },
           ],
         },
@@ -46,10 +73,14 @@ export default {
             {
               id: 1,
               name: "Wong",
+              timeIn: "123",
+              timeOut: "123",
             },
             {
               id: 2,
               name: "ABC",
+              timeIn: "123",
+              timeOut: "123",
             },
           ],
         },
@@ -59,10 +90,14 @@ export default {
             {
               id: 1,
               name: "Wong",
+              timeIn: "123",
+              timeOut: "123",
             },
             {
               id: 2,
               name: "ABC",
+              timeIn: "123",
+              timeOut: "123",
             },
           ],
         },
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import VCalendar from 'v-calendar';
 import BootstrapVue3 from 'bootstrap-vue-3'
+import print from 'vue3-print-nb'
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
@@ -17,4 +19,5 @@ createApp(App)
     // Use the components
     .use(BootstrapVue3)
     .use(VCalendar, {})
+    .use(print)
     .mount('#app')
